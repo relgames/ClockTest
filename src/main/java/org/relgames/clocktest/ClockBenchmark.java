fbbaba7bcc80c0fa535aa177a48ae0132f2f9bd6
relgames <relgames@gmail.com>
2012-05-09 17:06:49 +0200

    now with results returned

--- src/main/java/org/relgames/clocktest/ClockBenchmark.java
+++ src/main/java/org/relgames/clocktest/ClockBenchmark.java
@@ -7,16 +7,20 @@ import com.google.caliper.SimpleBenchmark;
  * @author relgames
  */
 public class ClockBenchmark extends SimpleBenchmark {
-    public void timeCurrentTimeMillis(int reps) {
+    public long timeCurrentTimeMillis(int reps) {
+        long result = 0;
         for (int i = 0; i < reps; i++) {
-            System.currentTimeMillis();
+            result ^= System.currentTimeMillis();
         }
+        return result;
     }
 
-    public void timeNanoTime(int reps) {
+    public long timeNanoTime(int reps) {
+        long result = 0;
         for (int i = 0; i < reps; i++) {
-            System.nanoTime();
+            result ^= System.nanoTime();
         }
+        return result;
     }
 
     public static void main(String[] args) throws Exception {
