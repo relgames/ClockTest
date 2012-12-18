3f492dc3d7a32e8f7d8b05937d17a5d4e8519e61
relgames <relgames@gmail.com>
2012-12-18 17:35:25 +0100

    removed nano test, added rdtsc check

--- src/main/java/org/relgames/clocktest/ClockBenchmark.java
+++ src/main/java/org/relgames/clocktest/ClockBenchmark.java
@@ -23,6 +23,7 @@ public class ClockBenchmark extends SimpleBenchmark {
         return result;
     }
 
+/*
     public long timeRdstc(int reps) {
         long result = 0;
         for (int i = 0; i < reps; i++) {
@@ -30,7 +31,7 @@ public class ClockBenchmark extends SimpleBenchmark {
         }
         return result;
     }
-
+*/
     public static void main(String[] args) throws Exception {
         Runner.main(ClockBenchmark.class, args);
     }
